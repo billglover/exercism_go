0657826363589e0bed5a45c75de3a8f69bcb791d
Bill Glover <bill@billglover.co.uk>
2017-08-13 08:43:19 +0100

    Refactor difference-of-squares for performance

--- difference-of-squares/difference_of_squares.go
+++ difference-of-squares/difference_of_squares.go
@@ -4,20 +4,16 @@ const testVersion = 1
 
 // SquareOfSums returns the square of the sums of the first
 // N numbers.
-func SquareOfSums(n int) (s int) {
-	for ; n > 0; n-- {
-		s += n
-	}
+func SquareOfSums(n int) int {
+	s := n * (1 + n) / 2
 	return s * s
 }
 
 // SumOfSquares returns the sum of the squares of the first
 // N numbers.
-func SumOfSquares(n int) (s int) {
-	for ; n > 0; n-- {
-		s += (n * n)
-	}
-	return s
+func SumOfSquares(n int) int {
+	// https://proofwiki.org/wiki/Sum_of_Sequence_of_Squares
+	return n * (n + 1) * (2*n + 1) / 6
 }
 
 // Difference returns the difference between the SquareOfSums
