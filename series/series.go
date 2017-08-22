e932a6e2fa7dda8da5738e84c106581544932c9b
Bill Glover <bill@billglover.co.uk>
2017-08-22 22:16:02 +0100

    Document exported functions

--- series/series.go
+++ series/series.go
@@ -2,6 +2,8 @@ package series
 
 const testVersion = 2
 
+// All returns all the contiguous substrings of length `n` from
+// a given a string of digits.
 func All(n int, s string) (series []string) {
 	for i := 0; i < len(s) && i+n <= len(s); i++ {
 		ns, ok := First(n, s[i:])
@@ -14,10 +16,15 @@ func All(n int, s string) (series []string) {
 	return series
 }
 
+// UnsafeFirst returns the first contiguous substrings of length `n` from
+// a given a string of digits. It does no bounds checking and will crash
+// if `n` is greater than the length of the string.
 func UnsafeFirst(n int, s string) (f string) {
 	return s[:n]
 }
 
+// First returns the first contiguous substrings of length `n` from
+// a given a string of digits.
 func First(n int, s string) (f string, ok bool) {
 	if n > len(s) {
 		return f, false
