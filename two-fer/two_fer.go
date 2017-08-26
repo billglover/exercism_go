cc296858ac575cfd08ebb40f71bae44e002df4e2
Bill Glover <bill@billglover.co.uk>
2017-08-26 21:11:56 +0100

    Remove stubbed comments

--- two-fer/two_fer.go
+++ two-fer/two_fer.go
@@ -1,13 +1,9 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package twofer should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package twofer
 
 import "fmt"
 
-// ShareWith needs a comment documenting it.
+// ShareWith takes a name and returns how things
+// should be shared.
 func ShareWith(name string) string {
 	if name == "" {
 		name = "you"
