ff369b88f83814b803d9a62d2e5d413a2089dc92
Bill Glover <bill@billglover.co.uk>
2017-09-02 14:54:01 +0100

    Complete isogram exercise

---
+++ isogram/isogram.go
@@ -0,0 +1,24 @@
+package isogram
+
+import "unicode"
+
+const testVersion = 1
+
+// IsIsogram determines if a word or phrase is an isogram. The comparison is
+// case insensitive and ignores non letter characters.
+func IsIsogram(s string) bool {
+	a := []rune(s)
+	m := make(map[rune]bool, len(s))
+
+	for _, l := range a {
+		r := unicode.ToLower(l)
+		if unicode.IsLetter(r) {
+			if ok := m[r]; ok == true {
+				return false
+			}
+			m[r] = true
+		}
+	}
+
+	return true
+}
