85715ee8458ee7d2b38d51f21b5a2e2861a4c38c
Bill Glover <bill@billglover.co.uk>
2017-10-07 21:17:33 +0100

    Handle computed cells

--- react/react.go
+++ react/react.go
@@ -1,7 +1,5 @@
 package react
 
-import "log"
-
 const testVersion = 5
 
 type Sheet struct{}
@@ -13,14 +11,17 @@ func (s Sheet) CreateInput(v int) InputCell {
 }
 
 func (s Sheet) CreateCompute1(c Cell, f func(a int) int) ComputeCell {
-	cc := CC{}
+	cc := CC1{}
 	cc.c1 = c
-	cc.SetValue(f) // TODO: store the function rather than value
+	cc.f = f
 	return &cc
 }
 
 func (s Sheet) CreateCompute2(c1, c2 Cell, f func(a, b int) int) ComputeCell {
-	cc := CC{}
+	cc := CC2{}
+	cc.c1 = c1
+	cc.c2 = c2
+	cc.f = f
 	return &cc
 }
 
@@ -31,32 +32,46 @@ type C struct {
 
 // Value returns the value in a cell.
 func (c *C) Value() int {
-	//log.Printf("%+v", c)
 	return c.v
 }
 
 // SetValue takes an integer and stores
 // it as the value of an InputCell.
 func (c *C) SetValue(v int) {
-	log.Println("setting value to:", v)
 	c.v = v
 }
 
-// CC represents a computed cell.
-type CC struct {
+// CC1 represents a computed cell.
+type CC1 struct {
+	f  func(a int) int
 	c1 Cell
-	v  func(a int) int
 }
 
-func (cc *CC) SetValue(f func(a int) int) int {
-	cc.v = f
+func (cc *CC1) SetValue(f func(a int) int) int {
+	cc.f = f
 	c := cc.c1
-	return cc.v(c.Value())
+	return cc.f(c.Value())
 }
 
-func (cc *CC) Value() int {
+func (cc *CC1) Value() int {
 	c := cc.c1
-	return cc.v(c.Value())
+	return cc.f(c.Value())
+}
+
+// CC2 represents a computed cell.
+type CC2 struct {
+	f  func(a, b int) int
+	c1 Cell
+	c2 Cell
+}
+
+func (cc *CC2) SetValue(f func(a, b int) int) int {
+	cc.f = f
+	return cc.Value()
+}
+
+func (cc *CC2) Value() int {
+	return cc.f(cc.c1.Value(), cc.c2.Value())
 }
 
 type Callback struct {
@@ -67,7 +82,14 @@ func (cb Callback) Cancel() {
 
 }
 
-func (c *CC) AddCallback(f func(int)) Canceler {
+func (c *CC1) AddCallback(f func(int)) Canceler {
+	cb := Callback{
+		f: f,
+	}
+	return cb
+}
+
+func (c *CC2) AddCallback(f func(int)) Canceler {
 	cb := Callback{
 		f: f,
 	}
