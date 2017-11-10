fa567b7b353d7561977e2e94a46de8f934768ef9
Bill Glover <bill@billglover.co.uk>
2017-11-10 08:20:38 +0000

    Add String function to enable debugging

---
+++ custom-set/custom_set.go
@@ -0,0 +1,76 @@
+package stringset
+
+import (
+	"strings"
+)
+
+const (
+	testVersion = 4
+)
+
+type Set map[string]bool
+
+func New() Set {
+	ss := Set{}
+	return ss
+}
+
+func NewFromSlice(sl []string) Set {
+	ss := &Set{}
+	for _, s := range sl {
+		ss.Add(s)
+	}
+	return *ss
+}
+
+func Subset(s1, s2 Set) bool {
+	return false
+}
+
+func Disjoint(s1, s2 Set) bool {
+	return false
+}
+
+func Equal(s1, s2 Set) bool {
+	return false
+}
+
+func Intersection(s1, s2 Set) Set {
+	return nil
+}
+
+func Difference(s1, s2 Set) Set {
+	return nil
+}
+
+func Union(s1, s2 Set) Set {
+	return nil
+}
+
+func (ss Set) String() string {
+	s := "{"
+
+	vals := make([]string, len(ss))
+	i := 0
+	for v := range ss {
+		vals[i] = "\"" + v + "\""
+		i++
+	}
+
+	s += strings.Join(vals, ", ")
+
+	s += "}"
+	return s
+}
+
+func (ss *Set) Has(v string) bool {
+	return false
+}
+
+func (ss *Set) IsEmpty() bool {
+	return true
+}
+
+func (ss *Set) Add(s string) {
+	(*ss)[s] = true
+}
