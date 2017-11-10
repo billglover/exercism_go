0c08afca8268f2d75279f7d8464882c4047950ba
Bill Glover <bill@billglover.co.uk>
2017-11-10 21:21:36 +0000

    Document all exported functions

--- custom-set/custom_set.go
+++ custom-set/custom_set.go
@@ -1,3 +1,5 @@
+// Package stringset implements a custom set type. The stringset
+// takes string elements and provides typical set operations.
 package stringset
 
 import (
@@ -9,13 +11,17 @@ const (
 	testVersion = 4
 )
 
+// Set is a set of unique elements. Each element is a string
 type Set map[string]bool
 
+// New returns a new (empty) set.
 func New() Set {
 	ss := Set{}
 	return ss
 }
 
+// NewFromSlice takes a slice of strings and returns a new set
+// with each item in the slice representing one item in the set.
 func NewFromSlice(sl []string) Set {
 	ss := &Set{}
 	for _, s := range sl {
@@ -24,6 +30,7 @@ func NewFromSlice(sl []string) Set {
 	return *ss
 }
 
+// Subset returns true if all elements in s1 are also present in s2.
 func Subset(s1, s2 Set) bool {
 	for k := range s1 {
 		if ok := s2[k]; ok == false {
@@ -33,6 +40,7 @@ func Subset(s1, s2 Set) bool {
 	return true
 }
 
+// Disjoint returns true if both sets share no elements.
 func Disjoint(s1, s2 Set) bool {
 	for k := range s1 {
 		if ok := s2[k]; ok == true {
@@ -42,10 +50,13 @@ func Disjoint(s1, s2 Set) bool {
 	return true
 }
 
+// Equal returns true if both sets are equal.
 func Equal(s1, s2 Set) bool {
 	return reflect.DeepEqual(s1, s2)
 }
 
+// Intersection takes two Sets and returns a new Set containing the
+// elements that are present in both.
 func Intersection(s1, s2 Set) Set {
 	is := Set{}
 
@@ -58,6 +69,8 @@ func Intersection(s1, s2 Set) Set {
 	return is
 }
 
+// Difference takes two Sets and returns values from s1 that are
+// not present in s2.
 func Difference(s1, s2 Set) Set {
 	ds := Set{}
 
@@ -70,6 +83,8 @@ func Difference(s1, s2 Set) Set {
 	return ds
 }
 
+// Union takes two Sets and returns a new Set containing all
+// elements from both sets.
 func Union(s1, s2 Set) Set {
 	us := s2
 
@@ -80,31 +95,35 @@ func Union(s1, s2 Set) Set {
 	return us
 }
 
+// String returns a string representation of a Set.
 func (ss Set) String() string {
 	s := "{"
 
-	vals := make([]string, len(ss))
+	el := make([]string, len(ss))
 	i := 0
 	for v := range ss {
-		vals[i] = "\"" + v + "\""
+		el[i] = "\"" + v + "\""
 		i++
 	}
 
-	s += strings.Join(vals, ", ")
+	s += strings.Join(el, ", ")
 
 	s += "}"
 	return s
 }
 
+// Has returns true if a Set contains the provided element.
 func (ss *Set) Has(v string) bool {
 	_, has := (*ss)[v]
 	return has
 }
 
+// IsEmpty returns true if a set contains no elements.
 func (ss *Set) IsEmpty() bool {
 	return len(*ss) == 0
 }
 
+// Add takes a string and adds an element to the set.
 func (ss *Set) Add(s string) {
 	(*ss)[s] = true
 }
